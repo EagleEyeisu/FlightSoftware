6c979396d74866c4af9991023aed0ea767f84a45
EagleEyeisu <cxsmith@iastate.edu>
2016-10-24 18:11:14 -0500

    Update MotorTest.ino

--- MotorTest.ino
+++ MotorTest.ino
@@ -23,10 +23,9 @@ Servo ESC1; //Creates an Servo object
  * number on the Arduino
  */
 void setup() {
-  ESC1.attach(9); //Adds ESC to certain pin. arm();
+  ESC1.attach(9);  //Adds ESC to certain pin. arm();
   Serial.begin(9600);// Starts the out put to the serial monitor.
 }
-
 /********************Main Program****************************
  * The program's core begins below where it sets the speed  *                       
  * of the ESC to 0% throttle. It than proceeds to increment *
@@ -35,26 +34,27 @@ void setup() {
  * throttle power sent to the ESC/motor back to 0.          *
  ************************************************************/
 void loop() {
+  
   int speed; //Creates speed variable
-
-  //setSpeed(100); //Used to set throttle range of ESC
-  //delay(5000);   //Used to set throttle range of ESC
-  ESC1.write(9);   //Begins program by sending what is eqivalent to 0% throttle.
-  delay(8000);     
-
-  for(speed = 0; speed <= 126; speed += 1) {  //Cycles speed up to 70% power with appropriate time delay.
-    ESC1.write(speed);                        //Calls function setSpeed (above) and passes the variable speed to it.
-    Serial.println(speed);                    //Prints speed variable to the serial monitor.
-    delay(500);                               //Delays the program for one fifth of a second.
+//ESC1.write(180); //Used to set throttle range of ESC
+//Serial.println("180");
+//delay(5000);   //Used to set throttle range of ESC
+  ESC1.write(8);   //Begins program by sending what is eqivalent to 0% throttle.
+  delay(8000);   
+    
+  for(speed = 16; speed <= 126; speed += 1) {  //Cycles speed up to 70% power with appropriate time delay.
+    ESC1.write(speed);                         //Calls function setSpeed (above) and passes the variable speed to it.
+    Serial.println(speed);                     //Prints speed variable to the serial monitor.
+    delay(500);                                //Delays the program for one fifth of a second.
   }
   
-  delay(4000);                                //Holds at 70% power for 4 seconds.
-
-  for(speed = 126; speed > 0; speed -= 1) {   //Cycles speed down to 0% power with appropriate time delay.
-    ESC1.write(speed);                        //Calls function setSpeed (above) and passes the variable speed to it.
-    Serial.println(speed);                    //Prints speed variable to the serial monitor.
-    delay(500);                               //Delays the program for one fifth of a second.
+  delay(4000);                                  //Holds at 70% power for 4 seconds.
+  
+  for(speed = 126; speed >= 16; speed -= 1) {  //Cycles speed down to 0% power with appropriate time delay.
+    ESC1.write(speed);                         //Calls function setSpeed (above) and passes the variable speed to it.
+    Serial.println(speed);                     //Prints speed variable to the serial monitor.
+    delay(500);                                //Delays the program for one fifth of a second.
   }
 
-  delay(1000); //Delays program for 1 second until the loop() starts over again.
+ delay(1000); //Delays program for 1 second until the loop() starts over again.
 }
