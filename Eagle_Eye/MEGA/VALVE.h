315a92a8a9496b187908380c344a0a200daba257
JacobCline <clinejake59@gmail.com>
2018-10-24 17:00:43 -0500

    Created 2 files that will be used for the Valve system

---
+++ Eagle_Eye/MEGA/VALVE.h
@@ -0,0 +1,23 @@
+/**
+ * VALVE.h holds objects related to the use of the Valve.
+ */
+
+ 
+#ifndef VALVE_h
+#define VALVE_h
+
+#include <Arduino.h>
+
+class VALVE
+{
+  public:
+  
+  // Constructor
+  VALVE();
+  
+  /*-----------------------------------------------------------------------------*/
+  
+
+};
+
+#endif
