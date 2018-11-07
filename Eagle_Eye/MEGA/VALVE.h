38f8aede347f17bcd5aa5a59251aa70cd03c1b1e
JacobCline <clinejake59@gmail.com>
2018-11-07 16:56:08 -0600

    I added a method to open and close the valve and the logic to go with it

--- Eagle_Eye/MEGA/VALVE.h
+++ Eagle_Eye/MEGA/VALVE.h
@@ -14,11 +14,20 @@ class VALVE
   
   // Constructor
   VALVE();
-  
+  // This Opens the Valve.
+  open_valve();
+  // This Closes the valve.
+  close_valve();
+  // Manages Valve Logic.
+  manager();
   /*-----------------------------------------------------------------------------*/
   // Valve Control
   #define VALVE_CTRL 20
-
+  // This is going to control whether we open or close the valve.
+  // 0-close.
+  // 1-open.
+  int valve_switch = 0;
+  
 };
 
 #endif
